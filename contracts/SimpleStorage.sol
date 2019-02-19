6b608bfd7fb74acde8c4776ce506c2cebcd59217
Cpeixin <13657247+spartan-brent@users.noreply.github.com>
2019-02-19 13:28:34 +0800

    Update SimpleStorage.sol
    solidity 0.5.0  函数参数（string）和返回参数（string）都要添加上memory

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,36 +1,35 @@
 pragma solidity ^0.5.0;
 
 contract SimpleStorage {
-  // 留言结构体
-  struct Message {
-    string word; // 留言
-    address from; // 留言者地址
-    string timestamp ; // 留言unix时间戳
-  }
+    // 留言结构体
+    struct Message {
+        string word; // 留言
+        address from; // 留言者地址
+        string timestamp ; // 留言unix时间戳
+    }
 
-  //定义数组
-  Message[] private wordAdrr;
+    Message[] private wordArr;
 
-  /**
+    /**
      * 写入留言的方法
      */
-  function setWord(string s, string t) public {
-    wordArr.push(Message({
-      word: s,
-      from: msg.sender,
-      timestamp: t
-      }));
-  }
+    function setWord(string memory s, string memory t) public {
+        wordArr.push(Message({
+            word: s,
+            from: msg.sender,
+            timestamp: t
+        }));
+    }
 
-  /**
+    /**
      * 获取随机留言的方法
      */
-  function getRandomWord(uint random) public view returns (uint, string, address, string) {
-    if(wordArr.length==0){
-      return (0, "", msg.sender, "");
-    }else{
-      Message storage result = wordArr[random];
-      return (wordArr.length, result.word, result.from, result.timestamp);
+    function getRandomWord(uint random) public view returns (uint, string memory, address, string memory) {
+        if(wordArr.length==0){
+            return (0, "", msg.sender, "");
+        }else{
+            Message storage result = wordArr[random];
+            return (wordArr.length, result.word, result.from, result.timestamp);
+        }
     }
-  }
 }
